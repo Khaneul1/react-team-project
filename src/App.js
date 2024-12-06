d38a264a346ad8e2912b66b1f752d406e1b9b0ce
Khaneul1 <kosky1219@gmail.com>
2024-12-06 11:45:41 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import LoginPage from './page-48-login/LoginPage';
+import Page42 from './page-42-43/Page42';
+import Page43 from './page-42-43/Page43';
+import Page44 from './page-44-45/Page44';
+import Page45 from './page-44-45/Page45';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
-
-export default App;
