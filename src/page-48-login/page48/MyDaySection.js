ea5c20eb110c1dd9aebcdaa9ebb48d8c7639e953
Khaneul1 <kosky1219@gmail.com>
2024-12-07 17:02:34 +0900

    yeahahahahahah

--- src/page-48-login/page48/MyDaySection.js
+++ src/page-48-login/page48/MyDaySection.js
@@ -27,40 +27,45 @@ const MyDaySection = () => {
       <div className="my-day-table">
         <div className="row">
           <span>방문한 장소</span>
-          <input
+          <textarea
             type="text"
             value={dayInputs.visitedPlace}
             onChange={(e) => handleInputChange('visitedPlace', e.target.value)}
+            className="my-day-input-box"
           />
-          <span>오늘 만난 사람</span>
-          <input
+          <span className="diary-met-people">오늘 만난 사람</span>
+          <textarea
             type="text"
             value={dayInputs.metPeople}
             onChange={(e) => handleInputChange('metPeople', e.target.value)}
+            className="my-day-input-box"
           />
         </div>
         <div className="row">
           <span>구입한 물건</span>
-          <input
+          <textarea
             type="text"
             value={dayInputs.purchasedItems}
             onChange={(e) =>
               handleInputChange('purchasedItems', e.target.value)
             }
+            className="my-day-input-box"
           />
-          <span>지출 금액</span>
-          <input
+          <span className="diary-expenses">지출 금액</span>
+          <textarea
             type="text"
             value={dayInputs.expenses}
             onChange={(e) => handleInputChange('expenses', e.target.value)}
+            className="my-day-input-box"
           />
         </div>
         <div className="row">
           <span>오늘의 뉴스</span>
-          <input
+          <textarea
             type="text"
             value={dayInputs.todayNews}
             onChange={(e) => handleInputChange('todayNews', e.target.value)}
+            className="todayNews-input-box"
           />
         </div>
       </div>
