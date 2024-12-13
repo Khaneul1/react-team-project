558c1052d02229c3ee7246f03669828347ea4694
Khaneul1 <kosky1219@gmail.com>
2024-12-13 17:29:03 +0900

    complete..maybe

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from 'react';
+import { Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import LoginPage from './page-48-login/LoginPage';
+import Page42 from './page-42-43/Page42';
+import SignupPage from './page-48-login/SignUpPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/page42" element={<Page42 />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
