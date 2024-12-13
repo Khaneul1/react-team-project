32bbda820736475be52cfcb58a5036848afca386
Khaneul1 <kosky1219@gmail.com>
2024-12-13 20:02:49 +0900

    no

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import Page44 from './page-44-45/Page44';
+import Page45 from './page-44-45/Page45';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Page45 />
+    </div>
+  );
 }
 
 export default App;
